c62339b2dec12dcce5555553d2bb13fe717c28a8
“kenkanayama” <ken.kanayama@pastrue.biz>
2023-02-12 18:58:44 +0900

    コメントで挙動メモ

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -14,7 +14,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.props.squares[i]}
+        value={this.props.squares[i]}//配列のi番目の要素
         onClick={() => this.props.onClick(i)}
       />
     );
@@ -44,37 +44,37 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  //state初期化
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
+      history: [{squares: Array(9).fill(null),}],
       stepNumber: 0,
       xIsNext: true
     }
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);// state管理している現在のhistoryを取得（すべての入力記録が入っているもの）（2次元配列ぽい構成）
+    const current = history[history.length - 1];// 最新のhistoryの配列を取得（{squares: []}）
+    const squares = current.squares.slice();// 最新のhistoryの配列を取得（[]）
+    console.log("history", history);
+    console.log("current", current);
+    console.log("squares", squares);
+    if (calculateWinner(squares) || squares[i]) {//勝者判定でnullじゃない場合、押下したSquareにすでに値が入っている場合は後続処理を行わない（何も更新するものはない）
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
+      history: history.concat([{squares: squares,}]),// state管理のhistoryに新しい配列を追加する（{squares: []}）
+      stepNumber: history.length,// 手番を更新する
+      xIsNext: !this.state.xIsNext,// bool値を逆にする
     })
-    console.log(squares);
+    console.log("squares", squares);
   }
   jumpTo(step) {
     this.setState({
-      stepNumber: step,
+      stepNumber: step,// 手番を更新する。連動してBoardに渡す配列（どのSquareにnullかX,Oの値を入れるかの配列）が変わるので、ブラウザ上の表示も更新される
       xIsNext: (step % 2) === 0,
     })
   }
@@ -106,7 +106,7 @@ class Game extends React.Component {
         <div className="game-board">
           {this.state.stepNumber}
           <Board
-            squares={current.squares}
+            squares={current.squares}//現在（最新の）の配列内の中身（[]）。各要素と要素数にマッピングされる箇所に値（Squareのprops.value）が入る
             onClick={(i) => this.handleClick(i)}
           />
         </div>
@@ -114,6 +114,7 @@ class Game extends React.Component {
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
+        {this.state.history[this.state.history.length - 1].squares.join(',')}
       </div>
     );
   }
